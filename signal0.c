ef8732c5dd7b42e467b35a5bcce01428a4f8c61b
Janat Taerakul <janat.taerakul@gmail.com>
2021-05-26 00:20:30 +0900

    First commit

---
+++ signal0.c
@@ -0,0 +1,29 @@
+#include <signal.h> /* signal */
+#include <unistd.h> /* write */
+
+void handler(int sig) {
+    return;
+}
+
+void alarm_handler(int sig) {
+    write(STDERR_FILENO, "*", 1);
+    alarm(5);
+    return;
+}
+
+int main() {
+    /* Exercise 1: Implement here. */
+    signal(SIGINT, handler);
+    signal(SIGALRM, alarm_handler);
+
+    /* Exercise 2: Implement here. */
+    alarm(5);
+
+    /* Prints dots. */
+    for (;;) {
+        for (int j = 0; j < 1024 * 1024 * 500; j++)
+            ; /* busy loop */
+        write(STDERR_FILENO, ".", 1);
+    }
+    return 0;
+}
